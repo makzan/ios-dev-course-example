c7527c5bd52bb5093a7ad918cbf1728d8a8057cf
Thomas Seng Hin Mak <makzan@gmail.com>
2012-06-08 22:53:52 +0800

    Added table view example codes

---
+++ class7/SelectionDemo/SelectionDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SelectionDemo
+//
+//  Created by Freshman on 6/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
